723601c289f86a783af7e6f26eaa687fa8c75e13
Javier Garcia Fernandez <70642519+i12gafej@users.noreply.github.com>
2022-12-21 20:18:36 +0100

    Delete build directory

--- build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yabiel/Desktop/IS/eq102/src
-# Build directory: /home/yabiel/Desktop/IS/eq102/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("programa")
--- build/src/programa/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yabiel/Desktop/IS/eq102/src/programa
-# Build directory: /home/yabiel/Desktop/IS/eq102/build/src/programa
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
