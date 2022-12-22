ed44893e74de81f00bf383a8da7e753d231a17cf
Javier Garcia Fernandez <70642519+i12gafej@users.noreply.github.com>
2022-12-22 09:46:08 +0100

    Update ej1.c

--- PRACTICA3/ej1.c
+++ PRACTICA3/ej1.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
-#include <string.h>
-#include <semaphore.h>
+#include <time.h>
+#include <pthread.h>
 #include <time.h>
 #include <unistd.h>
-typedef struct {
-    int modelos_disponibles;
-    int modelo_elegido;
-    int cantidad;
-}NOTAS;
 
-int camisetas[] = {0,0,0,0,0};
+
+int camisetas[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  
 
 void rellenarVector(int v[], int modelos){
@@ -19,21 +14,17 @@ void rellenarVector(int v[], int modelos){
         v[i] = rand()%100 + 1;
     }
 }
-
-void * cliente(void* arg){
-    NOTAS * n; 
-    n = (NOTAS*) arg;
-    
-    n->modelo_elegido = rand()%5;
-    n->cantidad = rand()%10 + 1;
-    
+void * cliente(){
+    int i = rand()%20 + 1;
+    int cantidad = rand()%10 + 1;
+    printf("El comprador %ld va a adquirir %d camisetas del tipo %d\n", pthread_self(), cantidad, i);
     if(pthread_mutex_lock(&mutex)){
         perror("Error de semaforo");
         exit(EXIT_FAILURE);
     }
 
     //seccion crittica
-    camisetas[n->modelo_elegido - 1] -= n->cantidad;
+    camisetas[i - 1] = camisetas[i - 1] - cantidad;
     //fin seccion critica
     if(pthread_mutex_unlock(&mutex)){
         perror("Error de semaforo");
@@ -41,20 +32,17 @@ void * cliente(void* arg){
     }
     pthread_exit(NULL);
 }
-void * proveedor(void * arg){
-    NOTAS * n; 
-    n = (NOTAS*) arg;
-    
-    n->modelo_elegido = rand()%5;
-    n->cantidad = rand()%10 + 1;
-    
+void * proveedor(){
+    int i = rand()%20 + 1;
+    int cantidad = rand()%10 + 1;
+    printf("El vendedor %ld va a suministrar %d camisetas del tipo %d\n", pthread_self(), cantidad, i);
     if(pthread_mutex_lock(&mutex)){
         perror("Error de semaforo");
         exit(EXIT_FAILURE);
     }
 
     //seccion crittica
-    camisetas[n->modelo_elegido - 1] += n->cantidad;
+    camisetas[i - 1] = camisetas[i - 1] + cantidad;
     //fin seccion critica
     if(pthread_mutex_unlock(&mutex)){
         perror("Error de semaforo");
@@ -62,27 +50,28 @@ void * proveedor(void * arg){
     }
     pthread_exit(NULL);
 }
-int main(){
-    NOTAS  * m = malloc(sizeof(NOTAS));
-    int n;
+int main(int argc, char **argv){
+    int n, provs;
+    n = atoi(argv[1]);
+    provs = atoi(argv[2]);
+
     srand(time(NULL));
-
-    printf("Introduzca el numero de clientes\n-->");
-    scanf("%d", n);
+   
+    rellenarVector(camisetas, provs);
+    printf("ABRIMOS LA TIENDA\n------------------------------------------------------------\n");
+    for(int i = 0; i<provs; i++){
+        printf("- Camisetas del tipo %d : %d\n", i+1, camisetas[i]);
+    }
     pthread_t clientes[n];
-    printf("Introduzca el numero de proveedores\n-->");
-    scanf("%d", m->modelos_disponibles);
-    pthread_t proveedores[m->modelos_disponibles];
-
-    rellenarVector(camisetas, n);
-    for(int i = 0; i < n; i++){
-        if(pthread_create(clientes+1, NULL, (void*) cliente, (void *)&m)!=0){
+    pthread_t proveedores[provs];
+     for(int i = 0; i < n; i++){
+        if(pthread_create(&clientes[i], NULL, cliente, NULL)!=0){
             perror("Hilo fallido");
             exit(EXIT_FAILURE);
         }
     }
-    for(int i = 0; i < m->modelos_disponibles; i++){
-        if(pthread_create(proveedores+1, NULL, (void*) proveedor, (void *)&m)!=0){
+    for(int i = 0; i < provs; i++){
+        if(pthread_create(&proveedores[i], NULL, proveedor, NULL)!=0){
             perror("Hilo fallido");
             exit(EXIT_FAILURE);
         }
@@ -93,11 +82,14 @@ int main(){
             exit(EXIT_FAILURE);
         }
     }
-    for(int i = 0; i < m->modelos_disponibles; i++){
+    for(int i = 0; i < provs; i++){
         if(pthread_join(proveedores[i], NULL)!=0){
             perror("Hilo fallido");
             exit(EXIT_FAILURE);
         }
     }
-    
+    printf("CERRAMOS LA TIENDA\n------------------------------------------------------------\n");
+    for(int i = 0; i<provs; i++){
+        printf("- Camisetas del tipo %d : %d\n", i+1, camisetas[i]);
+    }
 }
